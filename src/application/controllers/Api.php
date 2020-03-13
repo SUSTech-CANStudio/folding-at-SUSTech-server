8f1a7cad31e67722a4b4a6a7adfa8282f583cd7d
mikechesterwang <52522981+mikechesterwang@users.noreply.github.com>
2020-03-13 16:02:07 +0800

    this one should be the final version. no modification anymoregit status

--- src/application/controllers/Api.php
+++ src/application/controllers/Api.php
@@ -6,7 +6,6 @@ class Api extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        $this->load->library('session');
         $this->load->library('cas');
 
         // Set user's selected language.
