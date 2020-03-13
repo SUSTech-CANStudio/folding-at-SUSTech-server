85885af7f8d39e95796933bf30f5cda55eaf34c0
mikechesterwang <52522981+mikechesterwang@users.noreply.github.com>
2020-03-13 14:15:43 +0800

    fixed a bug

--- src/application/config/database.php
+++ src/application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => Config::DB_USERNAME,
 	'password' => Config::DB_PASSWORD,
 	'database' => Config::DB_NAME,
-	'dbdriver' => 'sqlite3',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => TRUE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
