dc5b2145684ac92d005367a7ee881e90e704c8dd
mikechesterwang <52522981+mikechesterwang@users.noreply.github.com>
2020-03-13 15:58:43 +0800

    fixed a bug

--- src/application/controllers/Api.php
+++ src/application/controllers/Api.php
@@ -6,7 +6,6 @@ class Api extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('api_model');
         $this->load->library('session');
         $this->load->library('cas');
 
@@ -23,11 +22,11 @@ class Api extends CI_Controller {
     }
 
     public function login($key){
-        $this->api_model->login($key);
+        $this->model_login($key);
     }
 
     public function accessConfiguration($key){
-        if($this->api_model->accessConfiguration($key)){
+        if($this->model_accessConfiguration($key)){
             $this->output
                 ->set_content_type('application/json')
                 ->set_output(json_encode($this->getConfig()), TRUE);
@@ -65,5 +64,47 @@ class Api extends CI_Controller {
             }
         }
         $this->load->view('general/update', $view);
+    }
+
+    private function model_login($key){
+        $timestamp_datetime = new DateTime('NOW');
+        $timestamp = $timestamp_datetime->format('Y-m-d H:i:s');
+        $data = array(
+            'access_key' => $key,
+            'timestamp' => $timestamp 
+        );
+        $this->db->insert('fast_access', $data);
+    }
+
+    private function model_accessConfiguration($key){
+        $access = $this->db->select('
+            fast_access.timestamp AS time
+        ')
+        ->from('fast_access')
+        ->where('access_key', $key)
+        ->get()
+        ->row_array()['time'];
+
+        // the key is not correct
+        if($access == NULL){
+            return false;
+        }
+
+        // calculate time diff
+        $access_datetime = new DateTime($access);
+        $now_datetime = new DateTime('NOW');
+        $interval = $access_datetime->diff($now_datetime);
+        $d_yea = $interval->format('%Y');
+        $d_mon = $interval->format('%m');
+        $d_day = $interval->format('%d');
+        $d_hrs = $interval->format('%H');
+        $d_min = $interval->format('%i');
+        // if time diff is within one min, return key
+        $rtn = $d_yea == 0 && $d_mon == 0 && $d_day == 0 && $d_hrs == 0 & $d_min == 0;     
+
+        $this->db->where('access_key', $key);
+        $this->db->delete('fast_access');
+
+        return $rtn;
     }
 }
