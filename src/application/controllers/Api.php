8178d685a6fb872f62ab175c0b52f4cf7c16e9c4
mikechesterwang <52522981+mikechesterwang@users.noreply.github.com>
2020-03-13 16:03:01 +0800

    ultimate version: all set up

--- src/application/controllers/Api.php
+++ src/application/controllers/Api.php
@@ -8,16 +8,6 @@ class Api extends CI_Controller {
         parent::__construct();
         $this->load->library('cas');
 
-        // Set user's selected language.
-        if ($this->session->userdata('language'))
-        {
-            $this->config->set_item('language', $this->session->userdata('language'));
-            $this->lang->load('translations', $this->session->userdata('language'));
-        }
-        else
-        {
-            $this->lang->load('translations', $this->config->item('language')); // default
-        }
     }
 
     public function login($key){
